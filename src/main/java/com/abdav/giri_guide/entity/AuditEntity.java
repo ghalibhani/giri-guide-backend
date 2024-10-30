a6f6aae6a4095cb7b20d239104997fa5dfb91def
Wimas Harjoko <wimasharjoko@gmail.com>
2024-10-30 11:49:23 +0700

    audit entity

---
+++ src/main/java/com/abdav/giri_guide/entity/AuditEntity.java
@@ -0,0 +1,29 @@
+package com.abdav.giri_guide.entity;
+
+import jakarta.persistence.EntityListeners;
+import lombok.Data;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@EntityListeners(AuditingEntityListener.class)
+@Data
+public class AuditEntity {
+    @CreatedBy
+    private String createdBy;
+
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    @LastModifiedBy
+    private String lastModifiedBy;
+
+    @LastModifiedDate
+    private LocalDateTime lastModifiedDate;
+
+    private Boolean isDeleted = false;
+}
