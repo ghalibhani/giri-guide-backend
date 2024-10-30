c8acfa204e544da061fe972f1670cdc0e3c4c45a
Wimas Harjoko <wimasharjoko@gmail.com>
2024-10-30 10:26:05 +0700

    INITIAL COMMIT

---
+++ src/main/java/com/abdav/giri_guide/GiriGuideApplication.java
@@ -0,0 +1,13 @@
+package com.abdav.giri_guide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiriGuideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiriGuideApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abdav/giri_guide/GiriGuideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abdav.giri_guide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiriGuideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
