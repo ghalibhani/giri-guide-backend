4f0bbda5348837d63b5dfc274303083ac0e61052
Ichlalsul Bulqiah aka creditFool <bichlasulbulqiah@gmail.com>
2024-10-31 15:40:52 +0700

    create HikingPoint entity

---
+++ src/main/java/com/abdav/giri_guide/model/request/HikingPointRequest.java
@@ -0,0 +1,8 @@
+package com.abdav.giri_guide.model.request;
+
+public record HikingPointRequest(
+        String name,
+        String coordinate
+
+) {
+}
---
+++ src/main/java/com/abdav/giri_guide/model/response/HikingPointResponse.java
@@ -0,0 +1,8 @@
+package com.abdav.giri_guide.model.response;
+
+public record HikingPointResponse(
+        String id,
+        String mountainId,
+        String name,
+        String coordinate) {
+}
