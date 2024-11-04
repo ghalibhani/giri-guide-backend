39c54eca8b9e072496b26aab499d48996618b652
Margaretha Gok Asi Naibaho <margarethanaibaho2@gmail.com>
2024-11-04 15:37:06 +0700

    update .env.template

--- src/main/java/com/abdav/giri_guide/controller/AuthController.java
+++ src/main/java/com/abdav/giri_guide/controller/AuthController.java
@@ -2,10 +2,7 @@ package com.abdav.giri_guide.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.abdav.giri_guide.constant.Message;
 import com.abdav.giri_guide.constant.PathApi;
