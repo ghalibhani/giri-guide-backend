a22740f6dba84d6e320774d36b8fc27e206ec341
Ichlalsul Bulqiah aka creditFool <bichlasulbulqiah@gmail.com>
2024-11-09 15:01:43 +0700

    update API endpoint location node by add property distance and fix bug where first node not saved

--- src/main/java/com/abdav/giri_guide/model/request/LocationRouteNodeRequest.java
+++ src/main/java/com/abdav/giri_guide/model/request/LocationRouteNodeRequest.java
@@ -3,10 +3,11 @@ package com.abdav.giri_guide.model.request;
 import jakarta.validation.constraints.NotNull;
 
 public record LocationRouteNodeRequest(
-        @NotNull String from,
-        @NotNull String to,
-        @NotNull String transportation,
-        @NotNull String estimate
+                @NotNull String from,
+                @NotNull String to,
+                @NotNull String transportation,
+                @NotNull String estimate,
+                @NotNull String distance
 
 ) {
 }
