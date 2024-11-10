18fe92a615b0b3b16ddc9d376977db29da653662
Ichlalsul Bulqiah aka creditFool <bichlasulbulqiah@gmail.com>
2024-11-10 11:58:49 +0700

    create delete endpoint for location route

--- src/main/java/com/abdav/giri_guide/controller/LocationRouteController.java
+++ src/main/java/com/abdav/giri_guide/controller/LocationRouteController.java
@@ -1,6 +1,16 @@
 package com.abdav.giri_guide.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.abdav.giri_guide.constant.Message;
@@ -11,16 +21,6 @@ import com.abdav.giri_guide.model.response.CommonResponse;
 import com.abdav.giri_guide.service.LocationRouteService;
 
 import lombok.RequiredArgsConstructor;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping(PathApi.LOCATION_ROUTE_API)
@@ -64,6 +64,14 @@ public class LocationRouteController {
                 .body(new CommonResponse<>(
                         locationRouteId,
                         locationRouteService.updateRoute(locationRouteId, request)));
+    }
+
+    @DeleteMapping("{locationRouteId}")
+    public ResponseEntity<?> deleteRoute(@PathVariable String locationRouteId) {
+        locationRouteService.deleteRoute(locationRouteId);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(new CommonResponse<>(
+                        Message.SUCCESS_DELETE, null));
     }
 
 }
